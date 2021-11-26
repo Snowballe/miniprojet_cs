656d8b97c4c3777c5224b939756bd59aa04beb74
Snowballe <62845660+Snowballe@users.noreply.github.com>
2021-11-26 16:15:29 +0100

    pouce dans creeper

---
+++ miniprojet_potcommun/Personne.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miniprojet_potcommun
+{
+    class Personne
+    {
+        public string prenom { get; set; }
+        public int argentDepense { get; set; }
+    }
+}
--- miniprojet_potcommun/Program.cs
+++ miniprojet_potcommun/Program.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace miniprojet_potcommun
 {
-    internal class Program
+    internal class Program : Personne
     {
         public static void Main(string[] args)
         {
+            Personne p = new Personne();
+            Console.WriteLine("Bonjour et bienvenue sur CommuMoney ! Qui participera au projet ? (ctrl pour quitter, alt pour finir)");
+
+            string[] participants=new string[64];
+            
+
+                for (int k = 0; k < 64; k++)
+                {
+                    Console.WriteLine("Nom numéro {0}", k + 1);
+                    participants[k] = (Console.ReadLine());
+                }
+
+            
+            
             
         }
     }
